be9816e95e7fa04218920756f858406647e2baf9
DmitryKorobko <korobko.dmitry@gmail.com>
2018-10-19 17:55:55 +0300

    Added contact and light refactoring for specs

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
+  let(:base_title) { "Chubakka" }
+
   describe "Home Page" do
     it "should have the content 'Chubakka'" do
       visit '/static_pages/home'
@@ -9,7 +11,7 @@ RSpec.describe "StaticPages", type: :request do
 
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Chubakka | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -21,7 +23,7 @@ RSpec.describe "StaticPages", type: :request do
 
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Chubakka | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -33,7 +35,19 @@ RSpec.describe "StaticPages", type: :request do
 
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("Chubakka | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+
+  describe "Help Page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
