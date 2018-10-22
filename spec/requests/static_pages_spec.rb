16dd6c702f61c64585c69795273fdc10cf1d39ad
DmitryKorobko <korobko.dmitry@gmail.com>
2018-10-22 12:02:21 +0300

    Light additions

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,9 +9,14 @@ RSpec.describe "StaticPages", type: :request do
       expect(page).to have_content('Chubakka')
     end
 
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} | Home")
+      expect(page).to have_title('Chubakka')
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).to_not have_title("#{base_title} | Home")
     end
   end
 
