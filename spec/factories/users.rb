9f2fb1dc18fc832762c19d3415c3aa9728dffc70
DmitryKorobko <korobko.dmitry@gmail.com>
2018-10-23 17:36:42 +0300

    Finish user signup

---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  sequence :email do |n|
+    "user#{n}@test.com"
+  end
+  factory :user do
+    name { 'Test User' }
+    email
+    password { '12345678' }
+    password_confirmation { '12345678' }
+  end
+end
