5493547a432690aae627a663214b031d3f828d86
DmitryKorobko <korobko.dmitry@gmail.com>
2018-10-29 12:02:55 +0200

    Fixed Heroku

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,8 @@ module Chubakka
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
+    # config/application.rb
+    config.assets.initialize_on_precompile = false
   end
 end
